4d3b423b68a379515e1c78d1371208698861762b
smit sojitra <82524813+smit153@users.noreply.github.com>
2021-12-25 18:27:48 +0530

    add solutions of array questions

--- Array/11.Count Items Matching a Rule.cpp
+++ Array/11.Count Items Matching a Rule.cpp
@@ -5,7 +5,6 @@ public:
         if(ruleKey=="type")i=0;
         if(ruleKey=="color")i=1;
         if(ruleKey=="name")i=2;
-        
         int ans=0;
         for(int j=0;j<items.size();j++){
             if(items[j][i]==ruleValue)ans++;
